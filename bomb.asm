8a6c14b4d47a9aedbb2998cecc3da6ad28fa11dc
tausiftt5238 <tausiftt5238@gmail.com>
2015-11-25 20:14:44 +0600

    bomb collision created

--- bomb.asm
+++ bomb.asm
@@ -6,6 +6,8 @@ extern draw_bmb:near
 extern fire_x:word, fire_y:word
 extern draw_explode:near
 extern temp_x:word, temp_y:word
+extern life:word
+extern c_tx:word,c_ty:word,c_alive:word,c_index:word
 
 public set_bomb
 public clear_bomb
@@ -99,6 +101,15 @@ bomb_blast_check_right:
 	cmp bomb_life,0
 	jl bomb_blast_right_clear
 	call draw_explode
+	call kill_creep
+	mov ax,b_tx
+	cmp ax,fire_x
+	jne bomb_blast_ck_rgt_kill_skip
+	mov ax,b_ty
+	cmp ax,fire_y
+	jne bomb_blast_ck_rgt_kill_skip
+	dec life
+bomb_blast_ck_rgt_kill_skip:
 	jmp bomb_blast_right_clear_skip
 bomb_blast_right_clear:
 	call clear_explosion
@@ -124,6 +135,15 @@ bomb_blast_check_left:
 	cmp bomb_life,0
 	jl bomb_blast_left_clear
 	call draw_explode
+	call kill_creep
+	mov ax,b_tx
+	cmp ax,fire_x
+	jne bomb_blast_ck_lft_kill_skip
+	mov ax,b_ty
+	cmp ax,fire_y
+	jne bomb_blast_ck_lft_kill_skip
+	dec life
+bomb_blast_ck_lft_kill_skip:
 	jmp bomb_blast_left_clear_skip
 bomb_blast_left_clear:
 	call clear_explosion
@@ -150,6 +170,15 @@ bomb_blast_check_up:
 	cmp bomb_life,0
 	jl bomb_blast_up_clear
 	call draw_explode
+	call kill_creep
+	mov ax,b_tx
+	cmp ax,fire_x
+	jne bomb_blast_ck_up_kill_skip
+	mov ax,b_ty
+	cmp ax,fire_y
+	jne bomb_blast_ck_up_kill_skip
+	dec life
+bomb_blast_ck_up_kill_skip:
 	jmp bomb_blast_up_clear_skip
 bomb_blast_up_clear:
 	call clear_explosion
@@ -176,6 +205,15 @@ bomb_blast_check_down:
 	cmp bomb_life,0
 	jl bomb_blast_down_clear
 	call draw_explode
+	call kill_creep
+	mov ax,b_tx
+	cmp ax,fire_x
+	jne bomb_blast_ck_dwn_kill_skip
+	mov ax,b_ty
+	cmp ax,fire_y
+	jne bomb_blast_ck_dwn_kill_skip
+	dec life
+bomb_blast_ck_dwn_kill_skip:
 	jmp bomb_blast_down_clear_skip
 bomb_blast_down_clear:
 	call clear_explosion
@@ -190,5 +228,27 @@ bmb_chk_down_done:
 	load_reg
 	ret
 bomb_blast endp
+
+kill_creep proc
+	save_reg
+	mov c_index,0
+	
+	mov cx,3
+kill_creep_loop:
+	mov si,c_index
+	mov ax,fire_x
+	cmp c_tx[si],ax
+	jne kill_creep_loop_skip
+	mov ax,fire_y
+	cmp c_ty[si],ax
+	jne kill_creep_loop_skip
+	mov c_alive,0
+kill_creep_loop_skip:
+	add c_index,2
+	loop kill_creep_loop
+	
+	load_reg
+	ret
+kill_creep endp
 
 end
