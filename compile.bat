f2942674f49b3efb25d17b3aff2fbb1f0034e115
sayon794 <sayonchy@gmail.com>
2015-11-27 00:35:18 +0600

    lead.asm added, contains bsort

--- compile.bat
+++ compile.bat
@@ -7,4 +7,5 @@ masm timer.asm
 masm bomb.asm
 masm creep.asm
 masm str.asm
-link main.obj rect.obj drawMap.obj setint.obj keyint.obj timer.obj bomb.obj creep.obj str.obj
+masm lead.asm
+link main.obj rect.obj drawMap.obj setint.obj keyint.obj timer.obj bomb.obj creep.obj str.obj lead.obj
---
+++ lead.asm
@@ -0,0 +1,79 @@
+extern names:word,scores:word,l_index1:byte,l_index2:byte
+
+public load_lead
+public store_lead
+public bsort
+
+.model small
+include mac
+.code
+load_lead proc
+	save_reg
+	
+	load_reg
+	ret
+load_lead endp
+
+store_lead proc
+	save_reg
+	
+	load_reg
+	ret
+store_lead endp
+
+bsort proc
+	save_reg
+	
+	mov si,-1
+bsort_outer:
+	inc si
+	cmp si,5
+	jmp bsort_done
+	mov di,4
+bsort_inner:
+	shl si,1
+	shl di,1
+	mov al,scores[si]
+	cmp al,scores[di]
+	jle bsort_skip
+	mov ah,scores[di]
+	mov scores[si],ah
+	mov scores[di],al			;swapping scores
+	shr si,1
+	shr di,1
+	
+	push si
+	push di
+	mov ax,si
+	mov bl,4
+	mul bl
+	mov ax,si
+	mov ax,di
+	mov bl,4
+	mul bl
+	mov ax,di
+	mov cx,3
+bsort_nameswap:
+	mov al,names[si]
+	mov ah,names[di]
+	mov names[si],ah
+	mov names[di],al
+	inc si
+	inc di
+	loop bsort_nameswap			;swapping names
+	pop di
+	pop si
+	
+bsort_skip:
+	dec di
+	cmp di,-1
+	jne bsort_inner
+	
+	jmp bsort_outer
+	
+bsort_done:
+	load_reg
+	ret
+bsort endp
+
+end
